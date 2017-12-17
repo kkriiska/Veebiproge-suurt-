d46e8575d7bf66c7ba054a4776f27ae390df95ca
Karolin Kriiska <kkriiska@gmail.com>
2017-12-17 13:29:42 +0200

    added content

--- data.php
+++ data.php
@@ -40,30 +40,23 @@
 <div class="container-fluid text-center">    
   <div class="row content">
     <div class="col-sm-2 sidenav">
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
     </div>
     <div class="col-sm-8 text-left"> 
-      <h1>Welcome</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h1>Tere tulemast!</h1>
+      <p>See leht on loodud selleks, et hoida enda porfooli jaoks vajalikke asju. Kui midagi oleks vaja lisada oma protfooliosse siis ma lisan selle siia ning hiljem saab korralikult panna porfooliosse. Lejht on vajalik ka selleks, et kui peaks olema olukord kus ma pean näitama oma portfooliot ja mu kõige uuemad tööd ei ole sinna ülespandud siis on neid hea näidata kuskil ilusas kohas tööandjale.</p>
       <hr>
-      <h3>Test</h3>
-      <p>Lorem ipsum...</p>
+	  <div class="h3-text">
+		<h3>Näidake mulle inimest, kes pole kordagi ühtegi viga teinud, ja ma näitan teile inimest, kes pole kunagi ka midagi erilist saavutanud.</h3>
+		<p></p>
+	  </div>
     </div>
     <div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>ADS</p>
-      </div>
-      <div class="well">
-        <p>ADS</p>
-      </div>
     </div>
   </div>
 </div>
 
 <footer class="container-fluid text-center">
-  <p>Footer Text</p>
+  <p>Karolin Kriiska if16 & Tatjana Kuznetsova if15</p>
 </footer>
 
 </body>
